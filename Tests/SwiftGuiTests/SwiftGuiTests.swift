97a1edd450262c0a8f4267a12bdd8531c189ca35
david-swift <haenerdavid@icloud.com>
2023-08-07 18:56:48 +0200

    Add macOS support

--- Tests/SwiftGuiTests/SwiftGuiTests.swift
+++ Tests/SwiftGuiTests/SwiftGuiTests.swift
@@ -7,6 +7,6 @@ final class SwiftGuiTests: XCTestCase {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct
     // results.
-    XCTAssertEqual(SwiftGui().text, "Hello, World!")
+    // XCTAssertEqual(SwiftGui().text, "Hello, World!")
   }
 }
