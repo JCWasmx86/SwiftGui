69be1e11919554019f526aa57ca663d429b78fea
david-swift <haenerdavid@icloud.com>
2023-08-15 20:58:25 +0200

    Add fields to widgets

--- Sources/GTUI/GTK/NativePeer.swift
+++ Sources/GTUI/GTK/NativePeer.swift
@@ -1,1 +1,4 @@
-open class NativePeer { public internal(set) var nativePtr: UInt64 = 0 }
+open class NativePeer {
+  public internal(set) var nativePtr: UInt64 = 0
+  public var fields: [String: Any] = [:]
+}
